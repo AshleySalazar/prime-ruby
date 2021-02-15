a07270c45ec42a4c92d137b2c8120a6490ddf161
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-02-14 22:27:23 -0600

    commit tree 4a0f8131b04a0d748ebf5f2f49ebac32a320cc3a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+    n = 2
+
+    return false if number <= 1
+
+    while n < number
+        return false if number % n == 0
+        n += 1
+    end
+
+    return true
+end
